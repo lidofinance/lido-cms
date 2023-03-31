85169b380ba68cbf539103b3b886ff7cd4692f30
Nikita Kozlov <nikita.kozlov@p2p.org>
2023-03-31 17:15:08 +0200

    feat: simple-oauth -> oktokit

--- pages/callback.tsx
+++ pages/callback.tsx
@@ -1,7 +1,7 @@
 import { useEffect, FC } from "react";
 import { GetServerSideProps } from "next";
 import getConfig from "next/config";
-import { oauth2 } from "widgets/auth";
+import { githubApp } from "widgets/auth";
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -13,18 +13,18 @@ interface CallbackProps {
 const Callback: FC<CallbackProps> = ({ message, content }) => {
   useEffect(() => {
     const oauthProvider = "github";
-    function recieveMessage(e: MessageEvent) {
-      console.log("recieveMessage %o", e);
-      const originHost = new URL(e.origin).host;
+    function recieveMessage(event: MessageEvent) {
+      console.log("recieveMessage %o", event);
+      const originHost = new URL(event.origin).host;
       const baseUrlHost = new URL(publicRuntimeConfig.baseUrl).host;
       if (originHost !== baseUrlHost) {
-        console.log("Invalid origin: %s", e.origin);
+        console.log("Invalid origin: %s", event.origin);
         return;
       }
       // send message to main window with da app
       window.opener?.postMessage(
         `authorization:${oauthProvider}:${message}:${JSON.stringify(content)}`,
-        e.origin
+        event.origin
       );
     }
     window.addEventListener("message", recieveMessage, false);
@@ -38,27 +38,23 @@ const Callback: FC<CallbackProps> = ({ message, content }) => {
 
 export default Callback;
 
-const getAccessToken = (options: Record<string, string>) =>
-  new Promise<{ message: string; content: any }>((resolve) => {
-    oauth2.authorizationCode.getToken(options, (error: Error, result: any) => {
-      if (error) {
-        console.error("Access Token Error", error);
-        resolve({
-          message: "error",
-          content: JSON.stringify(error.message),
-        });
-      } else {
-        const token = oauth2.accessToken.create(result);
-        resolve({
-          message: "success",
-          content: {
-            token: token.token.access_token,
-            provider: "github",
-          },
-        });
-      }
-    });
-  });
+const getAccessToken = async (options: { code: string }) => {
+  try {
+    const authenticationResult = await githubApp.createToken(options);
+    return {
+      message: "success",
+      content: {
+        token: authenticationResult.authentication.token,
+        provider: "github",
+      },
+    };
+  } catch (error) {
+    return {
+      message: "error",
+      content: JSON.stringify(error),
+    };
+  }
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   if (context.query.code == null || Array.isArray(context.query.code)) {
