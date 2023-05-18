d6dc472c5065ac570fef2dcdc83e0bbb3a57e702
Slavik191 <slava19970103@gmail.com>
2023-05-18 19:25:48 +0200

    feat: added ecosystem

---
+++ features/cms/initialize/collections/lidoLanding/files/ecosystem.ts
@@ -0,0 +1,28 @@
+import type { CmsCollectionFile } from "netlify-cms-core";
+
+export const ecosystem: CmsCollectionFile = {
+  name: "ecosystem",
+  label: "Ecosystem",
+  description: "Ecosystem page",
+  file: "lido-landing/ecosystem.md",
+  fields: [
+    {
+      name: "projects",
+      label: "Projects",
+      widget: "list",
+      label_singular: "new",
+      fields: [
+        { label: "Name", name: "name", widget: "string" },
+        { label: "Link", name: "link", widget: "string" },
+        { label: "Logo", name: "logo", widget: "image" },
+        { label: "Categories", name: "categories", widget: "select", options: ["Get", "Earn", "Hold", "Analyze", "Cover"], multiple: true},
+        { label: "Description", name: "description", widget: "string" },
+        { label: "Networks", name: "networks", widget: "select", options: ["ethereum", "solana", "polygon", "arbitrum", "optimism"], multiple: true },
+        { label: "Most popular", name: "mostPopular", widget: "boolean", default: false },
+        { label: "Tokens", name: "tokens", widget: "select", options: ["stETH", "wstETH", "stSOL", "stMATIC", "LDO"], multiple: true },
+        { label: "Learn more link", name: "learnMoreLink", widget: "string" },
+        { label: "Ready", name: "ready", widget: "boolean", default: false },
+      ]
+    }
+  ],
+};
--- features/cms/initialize/collections/lidoLanding/lidoLanding.ts
+++ features/cms/initialize/collections/lidoLanding/lidoLanding.ts
@@ -1,9 +1,13 @@
 import type { CmsCollection } from "netlify-cms-core";
 
 import { scorecard } from "./files/scorecard";
+import { ecosystem } from "./files/ecosystem";
 
 export const lidoLanding: CmsCollection = {
   name: "lido-landing",
   label: "Lido landing",
-  files: [scorecard],
+  files: [
+    scorecard,
+    ecosystem
+  ],
 };
