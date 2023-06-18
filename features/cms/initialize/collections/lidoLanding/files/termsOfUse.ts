2f2238fdb2e1301979f58a4b1a6cb8367d40f269
Yaroslav Denisov <jar1991@mail.ru>
2023-06-18 19:29:46 +0700

    feat: add policy page to cms

--- features/cms/initialize/collections/lidoLanding/files/termsOfUse.ts
+++ features/cms/initialize/collections/lidoLanding/files/termsOfUse.ts
@@ -6,16 +6,82 @@ export const termsOfUse: CmsCollectionFile = {
   description: "Terms of use page",
   file: "public/lido-landing/termsOfUse.md",
   fields: [
-    { label: "Timestamp", name: "timestamp", widget: "datetime" },
-    { label: "Title", name: "title", widget: "text" },
-    { label: "Precaution", name: "precaution", widget: "markdown", editor_components: ["terms-precaution"] },
     {
-      label: "Body",
-      name: "body",
+      label: "Timestamp",
+      name: "timestamp",
+      widget: "datetime",
+      format: "MMMM DD, YYYY",
+    },
+    { label: "Title", name: "title", widget: "text" },
+    {
+      label: "Short Info",
+      name: "shortInfo",
       widget: "markdown",
-      buttons: ["heading-two", "link"],
-      editor_components: ["scorecard-table"],
+      editor_components: [],
+      buttons: ["link", "bold"],
       modes: ["rich_text"],
+    },
+    {
+      label: "Precaution",
+      name: "precaution",
+      widget: "list",
+      fields: [
+        {
+          label: "Paragraph",
+          name: "paragraph",
+          widget: "text",
+        },
+      ],
+    },
+    {
+      label: "Terms of use heading",
+      name: "termsOfUseHeading",
+      widget: "text",
+    },
+    {
+      label: "Terms of use",
+      name: "termsOfUse",
+      widget: "list",
+      fields: [
+        {
+          label: "Paragraph",
+          name: "paragraph",
+          widget: "markdown",
+          modes: ["rich_text"],
+          buttons: ["bold", "link"],
+          editor_components: [],
+        },
+      ],
+    },
+    {
+      name: "bindingProvisionsHeading",
+      label: "Binding provisions heading",
+      widget: "text",
+    },
+    {
+      name: "bindingProvisions",
+      label: "Binding Provisions",
+      widget: "list",
+      fields: [
+        {
+          name: "header",
+          label: "Header",
+          widget: "text",
+        },
+        {
+          name: "paragraph",
+          label: "Paragraph",
+          widget: "markdown",
+          modes: ["rich_text"],
+          buttons: ["bold", "link", "numbered-list", "bulleted-list"],
+          editor_components: [],
+        },
+      ],
+    },
+    {
+      name: "footerText",
+      label: "Footer text",
+      widget: "text",
     },
   ],
 };
