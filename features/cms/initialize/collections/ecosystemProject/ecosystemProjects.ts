829deaed44299938c45a966712554b614896282c
Slavik191 <slava19970103@gmail.com>
2024-03-27 14:22:26 +0100

    fix: lint

--- features/cms/initialize/collections/ecosystemProject/ecosystemProjects.ts
+++ features/cms/initialize/collections/ecosystemProject/ecosystemProjects.ts
@@ -46,7 +46,12 @@ export const ecosystemProjects: CmsCollection = {
       widget: "string",
       required: false,
     },
-    { label: "L2 Description", name: "l2Description", widget: "string", required: false },
+    {
+      label: "L2 Description",
+      name: "l2Description",
+      widget: "string",
+      required: false,
+    },
     { label: "L2 Link", name: "l2Link", widget: "string", required: false },
     {
       label: "Tokens",
