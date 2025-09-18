6fcfe9bedc47a91fb35a43d85f5beaef6baf7f87
cakeinpanic <cakeinpanic@gmail.com>
2025-09-18 16:50:20 +0200

    feat: update rewards calculator component description in preview

--- features/cms/customWidgets/editorComponent/rewardsCalculator/index.ts
+++ features/cms/customWidgets/editorComponent/rewardsCalculator/index.ts
@@ -5,6 +5,6 @@ export const registerRewardsCalculator = () =>
     label: "Rewards calculator",
     fields: [],
     toPreview: function () {
-      return `<div style="padding: 10px; background: grey">Scorecard calculator component</div>`;
+      return `<div style="padding: 10px; background: grey">Rewards calculator component</div>`;
     },
   });
